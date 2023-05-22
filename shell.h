91201d17837b279ccec7df197bc02042d77377b5
valerienyachieo <valerienyachieo@gmail.com>
2023-05-22 00:54:36 -0700

    Header File

---
+++ shell.h
@@ -0,0 +1,16 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/type.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <signal.h>
+
+
+#endif
+
