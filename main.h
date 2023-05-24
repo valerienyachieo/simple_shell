b0b8807b3c16bfad4d7f7db9efabcf268e05b5ac
Naikumi <naikumimartha@gmail.com>
2023-05-24 14:51:54 -0700

    header file

--- main.h
+++ main.h
@@ -10,18 +10,17 @@
 #include <unistd.h>
 #include <signal.h>
 
+void prompt_and_read_input(char **con, ssize_t *ch, int value_fd);
+void handle_built_in_commands(char **tokenizado, char **environ);
+void execute_child_process(int argc, char *argv[], char **environ);
+void handle_parent_process(int *status, int value_fd);
+int main(int argc, char *argv[], char **environ);
 char *_path(char *con, char **environ);
 char *str_concate(char *str1, char *str2, char *str3);
 int _strcmp(char *str1, char *str2);
 int _strlen(char *s);
 char *_getenviron(char *name, char **environ);
-char *read_line(ssize_t *ch);
-char **words(char *line, char *separ);
-char **tokenization(char *input);
-int main(int argc, char **argv, char **environ);
-void exec_cmd(char *command, char **environment);
-void *_realloc(void *lineptr, size_t size);
-void *_memorycpy(void *dest, const void *src, size_t n);
-char *read_lines();
+char *read_lines(ssize_t *ch);
+char **words(char *lineptr, char *separ);
 
 #endif
